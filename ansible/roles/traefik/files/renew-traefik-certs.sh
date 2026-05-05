9b5c7df5f83caba3a2f5c3f46e439e19b7dcb656
dborshi <dborshi@outlook.com>
2026-05-05 17:27:29 +0200

    Proxy Cleanup

--- ansible/roles/traefik/files/renew-traefik-certs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cp /etc/letsencrypt/live/dbnet.work/fullchain.pem /etc/traefik/certs/fullchain.dbnet.crt
-cp /etc/letsencrypt/live/dbnet.work/privkey.pem   /etc/traefik/certs/wildcard.dbnet.key
-systemctl restart traefik
